53120145a4ba6e1d4a8bce0d2f5d764c683d8a38
mfmckenna <megan.mckenna@gmail.com>
2023-06-06 08:41:03 -0700

    added package

--- process_VesselDetections_TOL-1min_VSR_OC02.R
+++ process_VesselDetections_TOL-1min_VSR_OC02.R
@@ -21,6 +21,7 @@ library(BBmisc)
 library(zoo)
 library(stringr)
 library(plyr)
+library(viridis)
 
 # SETUP directories ####
 site = "OC02"
@@ -349,7 +350,7 @@ tal = as.data.frame( outputVD %>% group_by(Category) %>% tally() )
 # TIME LINE OF VD CONDITIONS
 ggplot(outputVD, aes(x=Start, xend=End, y=Category, yend=Category, color=`TOL_125 max`)) +
   geom_segment()+
-  theme_bw()+ 
+  theme_bw() + 
   scale_y_discrete(limits=rev)+ 
   geom_segment(size=12) +
   xlab("")+  ylab("")+ ggtitle("Summary of Vessel Detection Periods") +
@@ -358,5 +359,6 @@ ggplot(outputVD, aes(x=Start, xend=End, y=Category, yend=Category, color=`TOL_12
   theme(  axis.text.y = element_text(size = 14, colour="black"),
           axis.text.x=element_text(size = 14, colour="black"),
           plot.caption = element_text(size = 14) )
+
 # VD SAVE out ####
 save(outputVD,  file=paste0(outDir, "\\outputVD_", site , "_", DC, ".Rda") )
